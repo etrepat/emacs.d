fb0397592310c91559a2a07b5e63f8bc16c4b8bc
Estanislau Trepat <estanis@etrepat.com>
2015-07-24 20:16:54 +0200

    Add php-doc on php mode and a handy keyboard shortcut.

--- config/mode-php.el
+++ config/mode-php.el
@@ -8,6 +8,7 @@
 
 ;; php-mode
 (require 'php-mode)
+(require 'php-doc)
 
 (c-add-style
  "laravel"
@@ -26,7 +27,8 @@
   (c-set-style "laravel")
   (subword-mode +1)
   (add-to-list 'ac-sources 'ac-php-func-source)
-  (rainbow-delimiters-mode +1))
+  (rainbow-delimiters-mode +1)
+  (local-set-key (kbd "M-P") 'php-insert-doc-block))
 
 (setq php-mode-defaults-hook 'my-php-mode-defaults)
 
