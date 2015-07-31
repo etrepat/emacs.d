660c50a82c5cecb6bd39a0954e241279cc16a8d7
Estanislau Trepat <estanis@etrepat.com>
2015-07-31 10:50:13 +0200

    Minor tweaks to php-mode.

--- config/mode-php.el
+++ config/mode-php.el
@@ -20,13 +20,14 @@
 (defun my-php-mode-defaults ()
   "My PHP mode defaults."
   (setq php-template-compatibility nil
+        php-lineup-cascaded-calls t
         whitespace-line-column 120
         electric-pair-pairs '(
                               (?\{ . ?\})
                               ))
   (c-set-style "laravel")
   (subword-mode +1)
-  (add-to-list 'ac-sources 'ac-php-func-source)
+  ;; (add-to-list 'ac-sources 'ac-php-func-source)
   (rainbow-delimiters-mode +1)
   (local-set-key (kbd "M-P") 'php-insert-doc-block))
 
