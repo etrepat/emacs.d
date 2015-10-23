d8e74f0dd0d50dad7e4543534521c106af7873d5
Estanislau Trepat <estanis@etrepat.com>
2015-10-23 12:40:26 +0200

    Update ujelly theme.

--- themes/ujelly-theme.el
+++ themes/ujelly-theme.el
@@ -2,24 +2,24 @@
 
 ;; Author: Mark Tran <mark.tran@gmail.com>
 ;; URL: http://github.com/marktran/color-theme-ujelly
-;; Version: 1.0.46
+;; Version: 1.1.14
 
 ;; Inspired by jellybeans: http://www.vim.org/scripts/script.php?script_id=2555.
-(deftheme ujelly  "The ujelly color theme")
+(deftheme ujelly "The ujelly color theme")
 
 (let ((class '((class color) (min-colors 89)))
-      (ujelly-fg "#dfdfdf")
-      (ujelly-bg "#171717")
+      (ujelly-fg "#ffffff")
+      (ujelly-bg "#000000")
       (ujelly-blue-0 "#8fbfdc")
       (ujelly-green-0 "#99ad6a")
-      ; (ujelly-green-1 "#00FF00")
-      (ujelly-green-1 "#ffb964")
+      (ujelly-green-1 "#447799")
       (ujelly-green-2 "#a8ff60")
       (ujelly-grey-0 "#888888")
       (ujelly-grey-1 "#7f7f7f")
       (ujelly-grey-2 "#151515")
       (ujelly-grey-3 "#1c1c1c")
       (ujelly-grey-4 "#363636")
+      (ujelly-grey-5 "#444444")
       (ujelly-orange-0 "#ffb964")
       (ujelly-purple-0 "#8197bf")
       (ujelly-purple-1 "#474e90")
@@ -36,44 +36,15 @@
       (custom-theme-set-faces
        'ujelly
        `(default ((,class (:foreground ,ujelly-fg :background ,ujelly-bg))))
-       ;; `(sp-show-pair-match-face ((t (:background ,ujelly-bg :weight bold))))
-       ;;;;;
-       ;; show-paren-mode
-       ;;
-       ;;
-       `(show-paren-match-face ((t (:background ,ujelly-green-2 :weight bold :foreground ,ujelly-bg))))
-       ;;;;;
-       ;; company-mode
-       ;;
-       ;; company-tooltip                  - Face used for the tooltip
-       ;; company-tooltip-selection        - Face used for the selection in the tooltip
-       ;; company-tooltip-search           - Face used for the search string in the tooltip
-       ;; company-tooltip-mouse            - Face used for the tooltip item under the mouse
-       ;; company-tooltip-common           - Face used for the common completion in the tooltip
-       ;; company-tooltip-common-selection - Face used for the selected common completion in the tooltip
-       ;; company-tooltip-annotation       - Face used for the annotation in the tooltip
-       ;; company-scrollbar-fg             - Face used for the tooltip scrollbar thumb
-       ;; company-scrollbar-bg             - Face used for the tooltip scrollbar background
-       ;; company-preview                  - Face used for the completion preview
-       ;; company-preview-common           - Face used for the common part of the completion preview
-       ;; company-preview-search           - Face used for the search string in the completion preview
-       ;; company-echo                     - Face used for completions in the echo area
-       ;; company-echo-common              - Face used for the common part of completions in the echo area
+       `(alchemist-test--failed-face ((,class (:foreground ,ujelly-red-0))))
+       `(alchemist-test--success-face ((,class (:foreground ,ujelly-green-0))))
        `(company-preview-common ((,class (:foreground nil :background ,ujelly-purple-1))))
        `(company-scrollbar-bg ((,class (:background ,ujelly-grey-2))))
        `(company-scrollbar-fg ((,class (:background ,ujelly-grey-0))))
-       `(company-tooltip ((,class (:foreground ,ujelly-fg :background ,ujelly-grey-3))))
-       `(company-tooltip-common ((,class (:foreground ,ujelly-red-0 :background ,ujelly-grey-3))))
+       `(company-tooltip ((,class (:foreground ,ujelly-fg :background ,ujelly-grey-2))))
+       `(company-tooltip-common ((,class (:foreground ,ujelly-red-0 :background ,ujelly-grey-2))))
        `(company-tooltip-common-selection ((,class (:foreground ,ujelly-red-0 :background ,ujelly-purple-1))))
        `(company-tooltip-selection ((,class (:background ,ujelly-purple-1))))
-       ;;;;;
-       ;; alchemist
-       ;;
-       ;; alchemist-buffer--success-face  - Face for successful compilation run
-       ;; alchemist-buffer--failed-face   - Face for failed compilation run
-       `(alchemist-buffer--success-face ((t (:inherit font-lock-variable-name-face :bold t :background nil :foreground "darkgreen"))))
-       `(alchemist-buffer--failed-face ((t (:inherit font-lock-variable-name-face :bold t :background nil :foreground "red"))))
-
        `(compilation-error ((,class (:foreground ,ujelly-red-0))))
        `(compilation-info ((,class (:foreground ,ujelly-yellow-0))))
        `(compilation-line-number ((,class (:foreground ,ujelly-grey-0))))
@@ -95,6 +66,7 @@
        `(diredp-symlink ((,class (:foreground ,ujelly-red-3))))
        `(diredp-write-priv ((,class (:foreground ,ujelly-fg :background ,ujelly-bg))))
        `(emmet-preview-output ((,class (:background ,ujelly-purple-1))))
+       `(elixir-atom-face ((,class (:foreground ,ujelly-blue-0))))
        `(erc-notice-face ((,class (:foreground ,ujelly-yellow-0))))
        `(erc-prompt-face ((,class (:foreground ,ujelly-fg))))
        `(erc-timestamp-face ((,class (:foreground ,ujelly-purple-0))))
@@ -117,17 +89,23 @@
        `(font-lock-regexp-grouping-construct ((t (:foreground ,ujelly-yellow-0 :bold t))))
        `(font-lock-regexp-grouping-backslash ((t (:foreground ,ujelly-red-0 :bold t))))
        `(fringe ((,class (:foreground ,ujelly-fg :background ,ujelly-bg))))
-       `(git-commit-comment-file-face ((,class (:foreground ,ujelly-fg))))
-       `(git-commit-comment-heading-face ((,class (:foreground ,ujelly-yellow-0))))
-       `(git-commit-summary-face ((,class (:foreground ,ujelly-fg))))
+       `(git-commit-comment-file ((,class (:foreground ,ujelly-fg))))
+       `(git-commit-comment-heading ((,class (:foreground ,ujelly-yellow-0))))
+       `(git-commit-summary ((,class (:foreground ,ujelly-fg))))
        `(header-line ((,class (:foreground ,ujelly-fg))))
+       `(helm-buffer-file ((,class (:foreground ,ujelly-fg))))
+       `(helm-buffer-process ((,class (:foreground ,ujelly-yellow-0))))
        `(helm-buffer-size ((,class (:foreground ,ujelly-fg))))
        `(helm-candidate-number ((,class (:foreground ,ujelly-fg :background ,ujelly-bg))))
        `(helm-ff-directory ((,class (:background ,ujelly-bg))))
        `(helm-ff-file ((,class (:foreground ,ujelly-fg))))
-       `(helm-match ((,class (:foreground ,ujelly-yellow-0 :background ,ujelly-bg))))
+       `(helm-grep-lineno ((,class (:foreground ,ujelly-fg))))
+       `(helm-match ((,class (:foreground ,ujelly-red-1 :background ,ujelly-bg))))
+       `(helm-moccur-buffer ((,class (:foreground ,ujelly-yellow-0))))
        `(helm-selection ((,class (:background ,ujelly-purple-1))))
-       `(helm-source-header ((,class (:foreground ,ujelly-red-1 :background ,ujelly-grey-4))))
+       `(helm-source-header ((,class (:foreground ,ujelly-yellow-0 :background ,ujelly-grey-3))))
+       `(helm-swoop-target-line-face ((,class (:foreground ,ujelly-fg :background ,ujelly-grey-4))))
+       `(helm-swoop-target-word-face ((,class (:foreground ,ujelly-red-1))))
        `(hl-line ((,class (:background ,ujelly-grey-4))))
        `(isearch ((,class (:foreground ,ujelly-fg :background ,ujelly-red-1))))
        `(isearch-fail ((,class (:background ,ujelly-red-1))))
@@ -137,17 +115,25 @@
        `(ido-virtual ((,class (:foreground ,ujelly-purple-0))))
        `(lazy-highlight ((,class (:foreground ,ujelly-red-1 :background nil))))
        `(linum ((,class (:slant italic :foreground ,ujelly-grey-4))))
-       `(magit-branch ((,class (:foreground ,ujelly-red-0 :background nil))))
-       `(magit-diff-add ((,class (:foreground ,ujelly-green-0 :background ,ujelly-bg))))
-       `(magit-diff-del ((,class (:foreground ,ujelly-red-0 :background ,ujelly-bg))))
-       `(magit-diff-file-header ((,class (:inherit nil :foreground ,ujelly-red-2))))
-       `(magit-diff-hunk-header ((,class (:inherit nil :foreground ,ujelly-yellow-0))))
-       `(magit-item-highlight ((,class (:weight normal :background ,ujelly-grey-2))))
-       `(magit-log-author ((,class (:foreground ,ujelly-fg))))
-       `(magit-log-sha1 ((,class (:foreground ,ujelly-red-0))))
-       `(magit-log-head-label-local ((,class (:foreground ,ujelly-fg))))
-       `(magit-whitespace-warning-face ((,class (:background ,ujelly-red-1))))
-       `(magit-tag ((,class (:foreground ,ujelly-red-0 :background nil))))
+       `(magit-blame-heading ((,class (:foreground ,ujelly-grey-1 :background ,ujelly-grey-2))))
+       `(magit-branch-local ((,class (:foreground ,ujelly-green-2))))
+       `(magit-branch-remote ((,class (:foreground ,ujelly-green-2))))
+       `(magit-section-heading ((,class (:foreground ,ujelly-yellow-0 :background ,ujelly-grey-2))))
+       `(magit-diff-added ((,class (:foreground ,ujelly-green-0))))
+       `(magit-diff-added-highlight ((,class (:foreground ,ujelly-green-0 :inherit (magit-section-highlight)))))
+       `(magit-diff-context ((,class (:foreground ,ujelly-fg))))
+       `(magit-diff-context-highlight ((,class (:foreground ,ujelly-fg :inherit (magit-section-highlight)))))
+       `(magit-diff-file-heading ((,class (:weight normal :foreground ,ujelly-fg :background ,ujelly-bg))))
+       `(magit-diff-file-heading-highlight ((,class (:weight normal :foreground ,ujelly-fg :background ,ujelly-grey-2))))
+       `(magit-diff-hunk-heading ((,class (:foreground ,ujelly-yellow-0 :background ,ujelly-grey-2))))
+       `(magit-diff-hunk-heading-highlight ((,class (:foreground ,ujelly-yellow-0 :background ,ujelly-grey-3))))
+       `(magit-diff-lines-heading ((,class (:foreground ,ujelly-fg :background ,ujelly-purple-1))))
+       `(magit-diff-removed ((,class (:foreground ,ujelly-red-0))))
+       `(magit-diff-removed-highlight ((,class (:foreground ,ujelly-red-0 :inherit (magit-section-highlight)))))
+       `(magit-diffstat-added ((,class (:foreground ,ujelly-green-0))))
+       `(magit-diffstat-removed ((,class (:foreground ,ujelly-red-0))))
+       `(magit-hash ((,class (:foreground ,ujelly-red-1))))
+       `(magit-section-highlight ((,class (:background ,ujelly-grey-2))))
        `(match ((,class (:background ,ujelly-red-1))))
        `(minibuffer-prompt ((,class (:foreground ,ujelly-fg))))
        `(mode-line ((,class (:foreground ,ujelly-fg :background nil))))
@@ -158,11 +144,16 @@
        `(org-level-1 ((,class (:foreground ,ujelly-red-2))))
        `(org-level-2 ((,class (:foreground ,ujelly-red-0))))
        `(org-level-3 ((,class (:foreground ,ujelly-red-0))))
+       `(org-link ((,class (:foreground ,ujelly-blue-0))))
        `(org-special-keyword ((,class (:foreground ,ujelly-purple-0))))
        `(org-todo ((,class (:foreground ,ujelly-yellow-0))))
-       `(region ((,class (:background ,"gray20"))))
+       `(region ((,class (:background ,ujelly-purple-1))))
+       `(shm-current-face ((,class (:background ,ujelly-grey-4))))
+       `(shm-quarantine-face ((,class (:background ,ujelly-red-1))))
        `(smerge-markers ((,class (:foreground ,ujelly-yellow-0 :background ,ujelly-grey-2))))
        `(smerge-refined-change ((,class (:foreground ,ujelly-green-0))))
+       `(sp-pair-overlay-face ((,class (:background ,ujelly-grey-4))))
+       `(sp-show-pair-match-face ((,class (:background ,ujelly-grey-5))))
        `(trailing-whitespace ((,class (:background ,ujelly-red-1))))
        `(web-mode-builtin-face ((,class (:foreground ,ujelly-blue-0))))
        `(web-mode-html-attr-name-face ((,class (:foreground ,ujelly-purple-0))))
